f59ed33c8c5c38f99b36c7b3a6d0dc10d4df48cc
ahdad <78063619+ahdad@users.noreply.github.com>
2021-01-28 16:29:09 +0700

    Add files via upload

---
+++ jangan.py
@@ -0,0 +1,85 @@
+read -p '| | | | ____| |   | |   / _ \  / ___|| |   |  \/  |  / \|_   _|
+| |_| |  _| | |   | |  | | | | \___ \| |   | |\/| | / _ \ | |
+|  _  | |___| |___| |__| |_| |  ___) | |___| |  | |/ ___ \| |
+|_| |_|_____|_____|_____\___/  |____/|_____|_|  |_/_/   \_\_|
+
+ ____ _____  _    _   _  ____
+|  _ \_   _|/ \  | \ | |/ ___|
+| | | || | / _ \ |  \| | |  _
+| |_| || |/ ___ \| |\  | |_| |
+|____/ |_/_/   \_\_| \_|\____|' userinput1;
+#
+echo -e "\e[34mSILAKAN LOGIN TERLEBIH DAHULU KE AKUN FECEBOOK KALIAN\e[0m";
+read -p 'email:' userinput2;
+#
+read -p 'pasword:' userinput3;
+#
+if [ "${userinput3:-}" = "" ]
+then
+	echo -e "\e[92makun anda tidak di temukan silakan login kembali\e[0m";
+fi
+read -p 'email:' userinput4;
+#
+read -p 'pasword:' userinput5;
+#
+if [ "${userinput5:-}" = "" ]
+then
+	echo -e "\e[92msuccess..\e[0m";
+fi
+echo -e "kalian berhasil login";
+read -p 'ketik (y) jika ingin lanjut:' userinput6;
+#
+if [ "${userinput6:-}" = "y" ]
+then
+	clear
+fi
+echo -e "\e[91m____ ___   ___  _       _   _    _    ____ _  __  _____ ____
+|_   _/ _ \ / _ \| |     | | | |  / \  / ___| |/ / |  ___| __ )
+  | || | | | | | | |     | |_| | / _ \| |   | ' /  | |_  |  _ \
+  | || |_| | |_| | |___  |  _  |/ ___ \ |___| . \  |  _| | |_) |
+  |_| \___/ \___/|_____| |_| |_/_/   \_\____|_|\_\ |_|   |____/\e[0m";
+read -p 'login kembali me email anda:' userinput7;
+#
+echo -e "-------------------------- 
+081517994662
+Pasword: akulupa0
+Ip: 892.628.1.0
+--------------------------
+081575466228
+Paswors: bajingan28
+Ip: 826.836.27
+---------------------------
+081589053412
+Paswors: naisa987
+Ip:185.725.82
+---------------------------";
+echo -e "081577992552
+Paswors: nabil262
+Ip: 823.234.1.0
+---------------------------
+081566256373
+Paswors: arya946
+Ip: 123.732.8.0
+---------------------------
+081527363889
+Pasword: tolmans8
+Ip: 826.745.1.0
+---------------------------";
+echo -e "081547992317
+Pasword: bilikterbang
+Ip: 016.363.8.0
+---------------------------
+081563736739
+Pasword: hijaubangkuang82
+Ip: 082.333.8.0
+----------------------------
+081517998856
+pasword: bajingantol171
+ip:087.88.5
+----------------------------";
+read -p 'ketik ( ya ) jika ingin keluar dari tool:' userinput8;
+#
+if [ "${userinput8:-}" = "y" ]
+then
+	clear
+fi
